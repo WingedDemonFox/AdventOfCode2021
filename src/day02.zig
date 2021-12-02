86f52cb6c9e47ebb7e84df2a2ae7c9ad33ed29ee
WingedDemonFox <29842915+WingedDemonFox@users.noreply.github.com>
2021-12-02 17:29:28 +0100

    day 2 done

--- src/day02.zig
+++ src/day02.zig
@@ -11,8 +11,46 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day02.txt");
 
+const Submarine = struct{
+    horizontal: u64 = 0,
+    depth:u64 = 0,
+    aim:u64 =0,
+
+    pub fn SubMultiply(self:*Submarine) u64 {
+        return self.horizontal * self.depth;
+    }
+};
+
 pub fn main() !void {
-    
+    var part1Sub = Submarine{};
+    var part2Sub = Submarine{};
+
+    var lines = tokenize(u8,data, "\r\n");
+    while(lines.next()) |line|{
+        var tokenized = tokenize(u8, line, " ");
+        var order = tokenized.next().?;
+        var amount = try parseInt(u64, tokenized.next().?, 10);
+        
+        //std.debug.print("order: {s}, amount: {} \n", .{order, amount});
+        if(std.mem.eql(u8, order, "forward")){
+            part1Sub.horizontal += amount;
+            part2Sub.horizontal += amount;
+            part2Sub.depth += (amount * part2Sub.aim);
+
+        } else if(std.mem.eql(u8,order,"down")){
+            part1Sub.depth += amount;
+            part2Sub.aim += amount;
+
+        } else if(std.mem.eql(u8,order,"up")){
+            part1Sub.depth -= amount;
+            part2Sub.aim -= amount;
+        } else {
+            std.debug.print("something went horribly wrong", .{});
+        }
+    }
+
+    std.debug.print("Day 2 Part 1: {} \n", .{part1Sub.SubMultiply()});
+    std.debug.print("Day 2 Part 1: {} \n", .{part2Sub.SubMultiply()});
 }
 
 // Useful stdlib functions
